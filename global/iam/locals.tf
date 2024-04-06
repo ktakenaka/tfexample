6664251091b7bc52c1d197d315891b5602ff6f1a
Kazumasa Takenaka (Bamboo) <38031626+ktakenaka@users.noreply.github.com>
2024-04-06 22:59:48 +0700

    add iam role for github actions

---
+++ global/iam/locals.tf
@@ -0,0 +1,9 @@
+locals {
+  allowed_repos_branches = [
+    {
+      org    = "ktakenaka"
+      repo   = "terraformapprun"
+      branch = "main"
+    },
+  ]
+}
--- global/iam/main.tf
+++ global/iam/main.tf
@@ -62,3 +62,51 @@ resource "aws_iam_user_policy_attachment" "neo_cloudwatch_read_only_access" {
   user       = aws_iam_user.example[0].name
   policy_arn = aws_iam_policy.cloudwatch_read_only.arn
 }
+
+resource "aws_iam_role" "instance" {
+  name_prefix = "terraform-"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "ec2_admin_permission" {
+  statement {
+    effect = "Allow"
+    actions = ["ec2:*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy" "example" {
+  role = aws_iam_role.instance.id
+  policy = data.aws_iam_policy_document.ec2_admin_permission.json
+}
+
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+}
+
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect = "Allow"
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
+      type = "Federated"
+    }
+
+    condition {
+      test = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        for a in local.allowed_repos_branches : "repo:${a["org"]}/${a["repo"]}:ref:refs/heads/${a["branch"]}"
+      ]
+    }
+  }
+}
