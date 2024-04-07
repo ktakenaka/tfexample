7d7237a6c3dff37cca3d3b6a5e37aba8210499cf
Kazumasa Takenaka (Bamboo) <38031626+ktakenaka@users.noreply.github.com>
2024-04-07 11:13:45 +0700

    modify rds to use AWS secret

--- stage/data-stores/mysql/main.tf
+++ stage/data-stores/mysql/main.tf
@@ -24,36 +24,12 @@ resource "aws_db_instance" "example" {
   password = local.db_creds.password
 }
 
-data "aws_caller_identity" "self" {}
-
-data "aws_iam_policy_document" "cmk_admin_policy" {
-  statement {
-    effect    = "Allow"
-    resources = ["*"]
-    actions   = ["kms:*"]
-    principals {
-      type        = "AWS"
-      identifiers = [data.aws_caller_identity.self.arn]
-    }
-  }
-}
-
-resource "aws_kms_key" "cmk" {
-  policy = data.aws_iam_policy_document.cmk_admin_policy.json
-}
-
-resource "aws_kms_alias" "cmk" {
-  name          = "alias/kms-cmk-example"
-  target_key_id = aws_kms_key.cmk.id
-}
-
-data "aws_kms_secrets" "creds" {
-  secret {
-    name    = "db"
-    payload = file("./db-creds.yml.encrypted")
-  }
+data "aws_secretsmanager_secret_version" "creds" {
+  secret_id = "db-creds"
 }
 
 locals {
-  db_creds = yamldecode(data.aws_kms_secrets.creds.plaintext["db"])
+  db_creds = jsondecode(
+    data.aws_secretsmanager_secret_version.creds.secret_string
+  )
 }
